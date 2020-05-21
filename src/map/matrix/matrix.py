8699af7f39f9be0541825360d2cf563f62b383cb
LeoneBacciu <leonebacciu@gmail.com>
2020-05-21 16:12:32 +0200

    Add Cell to matrix.py

--- src/map/matrix/matrix.py
+++ src/map/matrix/matrix.py
@@ -1,14 +1,17 @@
 import numpy as np
 
+from src.map.matrix.cell import Cell
+
 
 class Matrix:
 
     def __init__(self, width):
         self._dims = (width, width)
-        self._matrix = np.ndarray(shape=self._dims, dtype=object)  # TODO: change to Cell
+        self._matrix = np.ndarray(shape=self._dims, dtype=Cell)
 
     def __getitem__(self, item):
         return self._matrix[item]
 
-    def __setitem__(self, key, value):  # TODO: add type check
-        self._matrix[key] = value
+    def __setitem__(self, key, value):
+        if type(value) == Cell:
+            self._matrix[key] = value
