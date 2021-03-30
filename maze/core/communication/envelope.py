f70e858b199136937f471fa21e039bc3947a6835
Leone Bacciu <leonebacciu@gmail.com>
2021-03-30 16:30:00 +0200

    Add envelope str representation

--- maze/core/communication/envelope.py
+++ maze/core/communication/envelope.py
@@ -11,6 +11,10 @@ class BaseInputEnvelope:
         walls = [bool(w) for w in data[1:5]]
         return cls(walls, *data[5:-1])
 
+    def __str__(self):
+        flags = {k: v for k, v in vars(self).items() if k != 'walls'}
+        return f'Walls: {str(self.walls)}, Flags: {str(flags)}'
+
 
 class BaseOutputEnvelope:
 
@@ -22,3 +26,7 @@ class BaseOutputEnvelope:
         direction[self.direction.value] = 1
         flags = [v for f, v in vars(self).items() if f != 'direction' and isinstance(v, int)]
         return bytes([START_TOKEN] + direction + flags + [STOP_TOKEN])
+
+    def __str__(self):
+        flags = {k: v for k, v in vars(self).items() if k != 'direction'}
+        return f'Direction: {str(self.direction)}, Flags: {str(flags)}'
